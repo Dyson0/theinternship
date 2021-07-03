a8eac029a51e6b9431218f85c6fe9cf1125bc8b9
DYSON <dysonsing@gmail.com>
2021-07-03 22:48:58 +0300

    work here

--- index.php
+++ index.php
@@ -1,4 +1,46 @@
 <?php
 
 
-echo("my index page")
+ $DISTRICTS = array(
+    'Wakiso' => array(
+   	     'Muvule' => 569,
+	     'Eucalyptus' => 80003,
+	     'Musizi' => 767363,
+	     'Musabu' => 8984,
+   ),
+   'Kabale'=> array(
+   	      'Muvule' => 69,
+	      'Eucalyptus' => 803,
+	      'Musizi' => 7363,
+	      'Musabu' => 89,
+   ),
+   'Kabale'=> array(
+   	       'Muvule' => 90,
+	       'Eucalyptus' => 703,
+	       'Musizi' => 763,
+	       'Musabu' => 29,
+   ),
+
+);
+
+
+// echo $DISTRICTS["wakiso"]["Muvule"];
+
+foreach ($DISTRICTS as $index => $value){
+
+     echo"<br>=====" .$index."=====<br>";
+    
+	echo"<br>Muvule=".$value['Muvule'];
+	echo"<br>Eucalyptus=". $value['Eucalyptus'];
+	echo"<br>Musizi=". $value['Musizi'];
+	echo"<br>Musabu=". $value['Musabu'];
+
+
+
+
+    
+
+
+
+
+}
--- master.php
+++ master.php
@@ -1,2 +1,30 @@
 <?php 
-echo "my master page";
+$multidymentionalArrays= array(
+   'Wakiso' => array(
+   	     'Muvule' => 569
+	     'Eucalyptus' => 80003
+	     'Musizi' => 767363
+	     'Musabu' => 8984
+   ),
+   'Kabale'=> array(
+   	      'Muvule' => 69
+	      'Eucalyptus' => 803
+	      'Musizi' => 7363
+	      'Musabu' => 89
+   ),
+   'Kabale'=> array(
+   	       'Muvule' => 90
+	       'Eucalyptus' => 703
+	       'Musizi' => 763
+	       'Musabu' => 29
+   ),
+
+);
+
+foreach ($multidymentionalArrays as $index => $value) {
+	echo"=====" .$index."=====<br>";
+	foreach ($value as $key => $trees) {
+		echo $key." = " .$trees."<br>";
+	}
+
+}
