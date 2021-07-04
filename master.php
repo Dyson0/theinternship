81ccd22d71e3004305c278025767fcc86df40c7a
Dyson0 <dysonsing@gmail.com>
2021-07-04 11:47:03 +0300

    work here

--- master.php
+++ master.php
@@ -1,2 +1,21 @@
 <?php 
+$sumofalldistricts=arrays(569,80003,767363,8984,69,703,763,29,69,803,7363,89);
 
+echo"-----sum(sumofalldistricts)-----= ".array_sum(sumofalldistricts),"----<br>;
+
+setcookie("sumofalldistricts","866887",time()+3600);
+
+if(isset($_cookie['sumofalldistricts'])){
+
+ echo $_cookie['sumofalldistricts'];
+
+ setcookie("sumofalldistricts","",-60);
+}
+
+else{
+	echo"---sum(sumofmasabu_trees)---","<br>";
+
+}
+
+$sumofmasabu_trees=array(8984,29,89);
+echo("-----sum(sumofmasabu_trees)----",array_sum($sumofmasabu_trees),-------<br>);
