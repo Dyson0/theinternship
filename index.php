034a8e1e96ed492d76fea429d092945247ce7a02
Dyson0 <dysonsing@gmail.com>
2021-07-05 14:14:57 +0300

    class work

--- index.php
+++ index.php
@@ -34,13 +34,48 @@ foreach ($DISTRICTS as $index => $value){
 	echo"<br>Eucalyptus=". $value['Eucalyptus'];
 	echo"<br>Musizi=". $value['Musizi'];
 	echo"<br>Musabu=". $value['Musabu'];
+}
 
+$sumofalltrees=arrays(569,80003,767363,8984,69,703,763,29,69,803,7363,89);
 
+echo"-----sum(sumofalltrees)-----= ".array_sum(sumofalldistricts),"----<br>;
 
+setcookie("sumofalltrees","866807",time()+3600);
 
-    
+if(isset($_cookie['sumofalltrees'])){
 
+ echo $_cookie['sumofalltrees'];
 
+ setcookie("sumofalltrees","",time()-60);
+}
 
+else{
+	echo"-------the cookie doesnt exist-----" ."<br>;
 
 }
+
+$sumofmasabu_trees=array(8984,29,89);
+echo("-----sum(sumofmasabu_trees)----",array_sum($sumofmasabu_trees),-------<br>);
+
+session_start();
+
+$_SESSION['sumofmasabu_trees']="9102";
+echo $_SESSION["sumofmasabu_trees"]. ",<br>";
+?>
+
+<DOCTYPE html>
+
+ <html>
+ 	<head>
+ 		meta charset="utf-8"
+ 		<title>The internship</title>
+ 	</head>
+ 	<body>
+ 		
+ 		<form method="POST" action "master.php">
+ 			<label>sumofalltrees</label>
+ 			<input type="number"name="">
+ 			<a href="master.php">Master page</a>
+ 	</body>
+
+</html>
--- master.php
+++ master.php
@@ -3,17 +3,17 @@ $sumofalldistricts=arrays(569,80003,767363,8984,69,703,763,29,69,803,7363,89);
 
 echo"-----sum(sumofalldistricts)-----= ".array_sum(sumofalldistricts),"----<br>;
 
-setcookie("sumofalldistricts","866887",time()+3600);
+setcookie("sumofalldistricts","873450",time()+3600);
 
 if(isset($_cookie['sumofalldistricts'])){
 
  echo $_cookie['sumofalldistricts'];
 
- setcookie("sumofalldistricts","",-60);
+ setcookie("sumofalldistricts","",time()-60);
 }
 
 else{
-	echo"---sum(sumofmasabu_trees)---","<br>";
+	echo"-------the cookie doesnt exist-----" ."<br>;
 
 }
 
