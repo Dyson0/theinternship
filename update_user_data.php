735cb73a5e7e3f84119cdaa347853f761b29ed87
Dyson0 <dysonsing@gmail.com>
2021-07-08 15:47:04 +0300

    work

--- update_user_data.php
+++
@@ -1,9 +0,0 @@
-<?php
-require('php_mysql.php');
-$names =$_POST['name_of_person'];
-
-$phone =$_POST['phone_number_of_person'];
-$user_id = $_POST['user_id'];
-$connection->query("UPDATE users SET NAME ='$names'PHONE_NUMBER ='$phone' WHERE ID = $user_id");
-
-header("location:user_form.php");
